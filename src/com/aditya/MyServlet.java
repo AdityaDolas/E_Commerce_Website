55cab9e5541b0a00c5d25bb4d3989ddfb1fb4cb9
Aditya Dolas <dolasadi@gmail.com>
2019-08-22 21:22:13 +0530

    Your message

---
+++ src/com/aditya/MyServlet.java
@@ -0,0 +1,63 @@
+package com.aditya;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out= response.getWriter();
+		String chkbox=request.getParameter("chkbox");
+		if(chkbox.equals("Add Cookie"))
+		{
+			Cookie c1=new Cookie("username", "admin");
+			Cookie c2=new Cookie("password", "admin");
+			c1.setMaxAge(360);
+			c2.setMaxAge(240);
+			
+			response.addCookie(c1);
+			response.addCookie(c2);
+			out.println("Cookie added successfully");
+		}
+		else
+		{
+			Cookie ck[]=request.getCookies();
+			for(Cookie c : ck)
+			{
+				out.println(c.getName()+" : "+c.getValue());
+			}
+		}
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
